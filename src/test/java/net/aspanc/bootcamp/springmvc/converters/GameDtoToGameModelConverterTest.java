2f9a5afbb9ceac0af18e3dbf63595796374bb7aa
lucx <lucx01.sb@gmail.com>
2023-04-26 11:11:34 +0000

    #7 Create game data converters tests

---
+++ src/test/java/net/aspanc/bootcamp/springmvc/converters/GameDtoToGameModelConverterTest.java
@@ -0,0 +1,42 @@
+package net.aspanc.bootcamp.springmvc.converters;
+
+import net.aspanc.bootcamp.springmvc.dtos.GameDto;
+import net.aspanc.bootcamp.springmvc.entities.GameModel;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameDtoToGameModelConverterTest {
+
+    private final String TEST_GAME_TITLE = "Game Test";
+    private final String TEST_GAME_DESCRIPTION = "Description Test";
+    private final Integer TEST_GAME_STEAMID = 1337;
+
+    private final GameDtoToGameModelConverter gameDtoToGameModelConverter = new GameDtoToGameModelConverter();
+
+    private GameDto gameDto;
+
+    @Before
+    public void setUp() {
+        gameDto = new GameDto();
+        gameDto.setId(Long.MIN_VALUE);
+        gameDto.setTitle(TEST_GAME_TITLE);
+        gameDto.setDescription(TEST_GAME_DESCRIPTION);
+        gameDto.setSteamId(TEST_GAME_STEAMID);
+    }
+
+    @Test
+    public void conversionTest() {
+        GameModel gameModel = gameDtoToGameModelConverter.convert(gameDto);
+
+        Assert.assertEquals(gameDto.getId(), gameModel.getId());
+        Assert.assertEquals(gameDto.getTitle(), gameModel.getTitle());
+        Assert.assertEquals(gameDto.getDescription(), gameModel.getDescription());
+        Assert.assertEquals(gameDto.getSteamId(), gameModel.getSteamId());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conversionTestNull() {
+        gameDtoToGameModelConverter.convert(null);
+    }
+}
---
+++ src/test/java/net/aspanc/bootcamp/springmvc/converters/GameModelToGameDtoConverterTest.java
@@ -0,0 +1,42 @@
+package net.aspanc.bootcamp.springmvc.converters;
+
+import net.aspanc.bootcamp.springmvc.dtos.GameDto;
+import net.aspanc.bootcamp.springmvc.entities.GameModel;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameModelToGameDtoConverterTest {
+
+    private final String TEST_GAME_TITLE = "Game Test";
+    private final String TEST_GAME_DESCRIPTION = "Description Test";
+    private final Integer TEST_GAME_STEAMID = 1337;
+
+    private final GameModelToGameDtoConverter gameModelToGameDtoConverter = new GameModelToGameDtoConverter();
+
+    private GameModel gameModel;
+
+    @Before
+    public void setUp() {
+        gameModel = new GameModel();
+        gameModel.setId(Long.MIN_VALUE);
+        gameModel.setTitle(TEST_GAME_TITLE);
+        gameModel.setDescription(TEST_GAME_DESCRIPTION);
+        gameModel.setSteamId(TEST_GAME_STEAMID);
+    }
+
+    @Test
+    public void conversionTest() {
+        GameDto gameDto = gameModelToGameDtoConverter.convert(gameModel);
+
+        Assert.assertEquals(gameModel.getId(), gameDto.getId());
+        Assert.assertEquals(gameModel.getTitle(), gameDto.getTitle());
+        Assert.assertEquals(gameModel.getDescription(), gameDto.getDescription());
+        Assert.assertEquals(gameModel.getSteamId(), gameDto.getSteamId());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void conversionTestNull() {
+        gameModelToGameDtoConverter.convert(null);
+    }
+}
