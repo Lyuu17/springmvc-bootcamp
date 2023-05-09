209ad19481ff0fbc8d4fc13b29c78454f3dfe03e
lucx <lucx01.sb@gmail.com>
2023-05-09 07:34:07 +0000

    #22 Enrich async game details and news

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   document.querySelectorAll('.delete-game-btn').forEach(elem => {
     elem.addEventListener('click', () => {
       if (confirm('Delete this game?')) {
@@ -13,7 +13,66 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  document.getElementById('add-game-btn').addEventListener('click', () => {
-    window.location.href = `/game/add`;
-  });
+  const addGameBtnElem = document.getElementById('add-game-btn');
+  if (addGameBtnElem) {
+    addGameBtnElem.addEventListener('click', () => {
+      window.location.href = `/game/add`;
+    });
+  }
+
+  const gameDataContainerColElem = document.getElementById('game-data-container-col');
+  if (gameDataContainerColElem) {
+    const gameScreenshotResponse = await fetch(`/game/steam/details/${gameDataContainerColElem.getAttribute('data-steamid')}`);
+    const { screenshotUrl } = await gameScreenshotResponse.json();
+
+    if (screenshotUrl !== undefined) {
+      gameDataContainerColElem.innerHTML = `
+      <div class="flex-column w-25 mb-4" id="game-image-column">
+        <div class="border rounded m-2">
+          <img width="100%" height="100%"
+               class="border rounded" src="${screenshotUrl}"
+               alt="Screenshot"/>
+        </div>
+      </div>
+      ${gameDataContainerColElem.innerHTML}`
+    }
+  }
+
+  const gameNewsContainerRowElem = document.getElementById('game-data-container-row');
+  if (gameNewsContainerRowElem) {
+    const gameScreenshotResponse = await fetch(`/game/steam/news/${gameNewsContainerRowElem.getAttribute('data-steamid')}`);
+    const newsData = await gameScreenshotResponse.json();
+
+    const { status } = newsData;
+
+    if (status === undefined && newsData.length > 0) {
+      let html = "";
+      newsData.forEach(data => {
+        const { title, url, author, contents } = data;
+
+        html += `
+          <div class="mb-3 border">
+            <div class="bg-success p-sm-2">
+              <h4><a class="text-decoration-none text-white" href="${url}">${title}</a></h4>
+            </div>
+            <div class="m-2">
+              <h5>${author}</h5>
+            </div>
+            <div class="m-2">
+              ${contents.replace(/{STEAM_CLAN_IMAGE}/g, "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans")}
+            </div>
+          </div>
+        `;
+      });
+
+      gameNewsContainerRowElem.innerHTML += `
+      <div class="d-flex flex-row m-2" id="game-news-container">
+        <div class="d-flex flex-column">
+          <h4>News</h4>
+          <div>${html}</div>
+        </div>
+      </div>
+      `;
+    }
+  }
 });
--- src/main/webapp/WEB-INF/jsp/game.jsp
+++ src/main/webapp/WEB-INF/jsp/game.jsp
@@ -5,7 +5,15 @@
 <tags:bootstrap/>
 <html>
 <head>
-  <title>Title</title>
+  <c:choose>
+    <c:when test="${empty game}">
+      <title><fmt:message key="label.game.notfound"/></title>
+    </c:when>
+    <c:otherwise>
+      <title>${game.title}</title>
+    </c:otherwise>
+  </c:choose>
+  <script type="text/javascript" src="/js/main.js"></script>
 </head>
 <body>
 
@@ -21,14 +29,20 @@
           <div class="bg-primary p-sm-2">
             <h2 class="text-white">${game.title}</h2>
           </div>
-          <div class="p-sm-2">
-            <ul class="list-group">
-              <li class="list-group-item">GameID: ${game.id}</li>
-              <li class="list-group-item">SteamID: ${game.steamId}</li>
-            </ul>
-          </div>
-          <div class="p-sm-3 border-top">
-            <p>${game.description}</p>
+          <div class="d-flex flex-column" id="game-data-container-row" data-steamid="${game.steamId}">
+            <div class="d-flex flex-row border-bottom" id="game-data-container-col" data-steamid="${game.steamId}">
+              <div class="flex-column w-75 mb-2 mt-2 border-start">
+                <div class="p-sm-2">
+                  <ul class="list-group">
+                    <li class="list-group-item">GameID: ${game.id}</li>
+                    <li class="list-group-item">SteamID: ${game.steamId}</li>
+                  </ul>
+                </div>
+                <div class="p-sm-3 border-top">
+                  <p>${game.description}</p>
+                </div>
+              </div>
+            </div
           </div>
         </div>
       </div>
