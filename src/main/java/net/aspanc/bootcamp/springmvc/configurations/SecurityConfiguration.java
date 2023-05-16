9333c2468f1864c88df297e041ce5c911ddb8f91
lucx <lucx01.sb@gmail.com>
2023-05-16 15:00:39 +0000

    #30 Enable required auth for /admin/rest/*** endpoint

--- src/main/java/net/aspanc/bootcamp/springmvc/configurations/SecurityConfiguration.java
+++ src/main/java/net/aspanc/bootcamp/springmvc/configurations/SecurityConfiguration.java
@@ -1,13 +1,33 @@
 package net.aspanc.bootcamp.springmvc.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Configuration
+    @EnableWebSecurity
+    public static class APIConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.csrf()
+                    .disable()
+                    .antMatcher("/admin/rest/**")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .httpBasic();
+        }
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
